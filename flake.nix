97ad0f0780fe824a593ec1eaf4f60aede2efa6c4
Alexandre Macabies <web+oss@zopieux.com>
2024-01-28 20:02:32 +0100

    Add nix flake.

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      packages = { };
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ nodejs ];
+      };
+    });
+}
