1c9fb3150a7b80350de5ab8c29672a31c280d1e4
Alexandre Macabies <web+oss@zopieux.com>
2024-01-28 20:01:19 +0100

    Remove electron crap.

--- electron/detect-platform.ts
+++
@@ -1,7 +0,0 @@
-import os from 'os';
-
-export const isDev = !!process.env.VITE_DEV_SERVER_URL
-
-export const isMac = os.platform() === "darwin";
-export const isWindows = os.platform() === "win32";
-export const isLinux = os.platform() === "linux";
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
-import { rmSync } from 'node:fs'
 import { join } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import electron from 'vite-plugin-electron'
-import renderer from 'vite-plugin-electron-renderer'
 import license from 'rollup-plugin-license'
 import pkg from './package.json'
 import path from 'path'
 
 import { keyHelpStr } from "./shared-utils/key-helper";
-
-rmSync('dist-electron', { recursive: true, force: true })
-
-const isDevelopment = process.env.NODE_ENV === "development" || !!process.env.VSCODE_DEBUG
-const isProduction = process.env.NODE_ENV === "production"
 
 const updateReadmeKeybinds = async () => {
 	const fs = require('fs')
@@ -48,70 +40,6 @@ export default defineConfig({
 	plugins: [
 		vue(),
 		updateReadmeKeybinds(),
-		electron([
-			{
-				// Main-Process entry file of the Electron App.
-				entry: 'electron/main/index.ts',
-				onstart(options) {
-					if (process.env.VSCODE_DEBUG) {
-						console.log(/* For `.vscode/.debug.script.mjs` */'[startup] Electron App')
-					} else {
-						options.startup()
-					}
-				},
-				vite: {
-					build: {
-						sourcemap: isDevelopment,
-						minify: isProduction,
-						outDir: 'dist-electron/main',
-						rollupOptions: {
-							external: Object.keys("dependencies" in pkg ? pkg.dependencies : {}),
-						},
-					},
-				},
-			},
-			{
-				entry: 'electron/preload/index.ts',
-				onstart(options) {
-					// Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete, 
-					// instead of restarting the entire Electron App.
-					options.reload()
-				},
-				vite: {
-					build: {
-						sourcemap: isDevelopment,
-						minify: isProduction,
-						outDir: 'dist-electron/preload',
-						rollupOptions: {
-							external: Object.keys("dependencies" in pkg ? pkg.dependencies : {}),
-						},
-					},
-				},
-			},
-			{
-				entry: 'electron/preload/about-preload.js',
-				onstart(options) {
-					// Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete, 
-					// instead of restarting the entire Electron App.
-					options.reload()
-				},
-				vite: {
-					build: {
-						sourcemap: isDevelopment,
-						minify: isProduction,
-						outDir: 'dist-electron/preload',
-						rollupOptions: {
-							external: Object.keys("dependencies" in pkg ? pkg.dependencies : {}),
-						},
-					},
-				},
-			}
-		]),
-		// Use Node.js API in the Renderer-process
-		renderer({
-			nodeIntegration: false, // turning this on will break Math.js
-		}),
-
 		// Add license header to the built files
 		license({thirdParty: {
 			output: join(__dirname, 'dist', 'dependencies.txt'),
